d58fedda568210f4e904942902c624b9c81f922d
smmk <smmk123@gmail.com>
2023-06-16 04:11:42 -0400

    save grid settings to local

--- src/components/UsersPage.jsx
+++ src/components/UsersPage.jsx
@@ -11,6 +11,22 @@ import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 
 const UsersPage = () => {
+  const layout = [
+    { i: 'latestUser', x: 0, y: 0, w: 3, h: 2, minH: 2, minW: 3 },
+    { i: 'usersLoggingIn', x: 3, y: 0, w: 3, h: 2 },
+    { i: 'kuriboh', x: 2, y: 0, w: 1, h: 1 },
+    { i: 'spell-caster', x: 3, y: 0, w: 1, h: 1 },
+    { i: 'summoned-skull', x: 4, y: 0, w: 1, h: 1 },
+  ];
+
+  const getLayouts = () => {
+    const savedLayouts = localStorage.getItem('grid-layout');
+
+    return savedLayouts ? JSON.parse(savedLayouts) : { lg: layout };
+  };
+  const handleLayoutChange = (layout, layouts) => {
+    localStorage.setItem('grid-layout', JSON.stringify(layouts));
+  };
   const [userStats, setUserStats] = useState(null);
   const ResponsiveGridLayout = WidthProvider(Responsive);
 
@@ -25,21 +41,21 @@ const UsersPage = () => {
         Users (Click and Drag to customize)
       </span>
       <ResponsiveGridLayout
+        layouts={getLayouts()}
         className="layout"
         breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
         cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
         rowHeight={200}
+        onLayoutChange={handleLayoutChange}
       >
         <div
           key="latestUser"
-          data-grid={{ x: 0, y: 0, w: 3, h: 2, minH: 2, minW: 3 }}
           className="bg-gradient-to-br from-pink-500 to-orange-500 text-white rounded-lg p-4"
         >
           <LatestUserDataTable />
         </div>
         <div
           key="usersLoggingIn"
-          data-grid={{ x: 3, y: 0, w: 3, h: 2 }}
           className="bg-gradient-to-br from-blue-500 to-cyan-400 text-white rounded-lg p-4"
         >
           {userStats && (
